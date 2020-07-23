089a8a2dc0820765dea4f346a3b95cc351a5ff20
Marcellus Tavares <marcellus.tavares@liferay.com>
2020-07-23 10:20:46 -0700

    add assets and activities script

---
+++ activities-data-loader.py
@@ -0,0 +1,30 @@
+from common import ElasticsearchBridge
+
+import random
+import datetime
+import argparse
+import sys
+import json
+
+argument_parser = argparse.ArgumentParser(
+    usage='{} activities_data_loader '
+    '--elasticsearch-hostname <Elasticsearch Hostname, Optional> -> default http://localhost:9200 '
+    '--lcp-project-id <LCP Project ID>'.format(sys.argv[0])
+)
+
+argument_parser.add_argument('--elasticsearch-hostname', default='http://localhost:9200')
+argument_parser.add_argument('--lcp-project-id', required=True)
+
+args = argument_parser.parse_args()
+
+elasticsearch_bridge = ElasticsearchBridge(
+    args.elasticsearch_hostname, args.lcp_project_id
+)
+
+with open("data/activities.json", "r") as f:
+    for line in f:
+        doc = json.loads(line)
+
+        elasticsearch_bridge.index(
+            doc, doc.get('id'), 'activities', 'osbasahfaroinfo'
+        )
